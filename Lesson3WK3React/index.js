11438ef201f71ccaa92430c827d35e15aa614c15
Leticia Gomez <137099040+lggomez18@users.noreply.github.com>
2024-06-13 20:59:17 +0000

    Created a new component to list cities.

---
+++ Lesson3WK3React/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import App from "Lesson3WK3React/App.css";
+
+export default App(){
+    return(
+        <div className="App">
+        <h1>React Loop </h1>
+        <cities />
+        </div>
+    );
+}
