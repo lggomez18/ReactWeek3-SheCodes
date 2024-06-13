b61d929b048522aa06c4c1dfbb4bafa5aebb3edb
Leticia Gomez <137099040+lggomez18@users.noreply.github.com>
2024-06-13 22:26:09 +0000

    Practicing how to create React Loops

---
+++ Lesson3WK3React/Baltic.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Baltic() {
+  return (
+    <ul>
+      <li>🇪🇪Estonia</li>
+      <li>🇱🇻Latvia</li>
+      <li>🇱🇹Lithuania</li>
+    </ul>
+  );
+}
+
+//Arrays are written inside []
+//In order to loop, list the arre in <li>
--- Lesson3WK3React/index.js
+++ Lesson3WK3React/index.js
@@ -1,12 +1,41 @@
-import React from "react";
-import App from "Lesson3WK3React/App.css";
-import cities from "Lesson3WK3React/cities";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-export default App(){
-    return(
-        <div className="App">
-        <h1>React Loop </h1>
-        <cities />
-        </div>
-    );
-}
+import "./styles.css";
+import Baltic from "./Baltic";
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+let countries = ["France", "USA", "Australia", "Japan"];
+root.render(
+  <StrictMode>
+    <div className="App">
+      <h1>React Loops</h1>
+
+      <h3>Challenge 1</h3>
+      <p>
+        Create a new Component called BalticCountries and list the 3 baltic
+        countries (Estonia, Latvia, Lithuania) without using loops or variables.
+        Include the component below.
+      </p>
+
+      <Baltic />
+
+      <h3>Challenge 2</h3>
+      <p>
+        Include a component called Countries below and send the variable
+        countries (created above) as a property. Create a new component called
+        Countries and list all countries from the Countries component using a
+        loop.
+      </p>
+
+      <h3>Challenge 3</h3>
+      <p>
+        Modify the countries variable in the code above so it includes the
+        country name but also the capital city of each country. Display each
+        country and capital city in the Countries component
+      </p>
+    </div>
+  </StrictMode>,
+);
