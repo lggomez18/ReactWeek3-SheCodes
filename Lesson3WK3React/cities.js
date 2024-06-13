fef9c7175f275fb161e650fa83d9ec7b58e3a5b1
Leticia Gomez <137099040+lggomez18@users.noreply.github.com>
2024-06-13 21:31:22 +0000

    Loop and arrays first introduction

--- Lesson3WK3React/cities.js
+++ Lesson3WK3React/cities.js
@@ -1,7 +1,19 @@
 import React from "react";
 
 export default function Cities() {
-    let city="Paris;
+  let city = ["Paris", "London", "Moscow", "Mexico"];
 
-  return <div className="Cities">{city}</div>;
+  return (
+    <div className="Cities">
+      <ul>
+        <li>Paris</li>
+        <li>London</li>
+        <li>Moscow</li>
+        <li>Mexico</li>
+      </ul>
+    </div>
+  );
 }
+
+//Arrays are written inside []
+//In order to loop, list the arre in <li>
